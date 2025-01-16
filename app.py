d7eacae40d815537c40022734cf63f29c4023a55
theiskander <iskander.burlai@gmail.com>
2025-01-16 18:40:11 +0200

    Created index route

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    translation = ""
+    if request.method == 'POST':
+        # Set up API
+        text = request.form.get('text')
+        source_lang = request.form.get('source_lang')
+        target_lang = request.form.get('target_lang')
+        url = "https://api.mymemory.translated.net/get"
+        params = {
+            'q': text,
+            'langpair': f"{source_lang}|{target_lang}"
+        }
+        
+        # Use API
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            translation = data['responseData']['translatedText']
+        except Exception as e:
+            translation = f"Unexpected error occured: {str(e)}"
     return render_template('index.html')
 
 if __name__ == "__main__":
